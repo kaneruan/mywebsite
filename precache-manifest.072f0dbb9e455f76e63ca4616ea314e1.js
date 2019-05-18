83ae51f59900df510b126e7ee2c2d7c80f6cb6a3
aronruan <aronruan@kugou.net>
2019-05-18 12:52:52 +0800

    Updates

---
+++ precache-manifest.072f0dbb9e455f76e63ca4616ea314e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003f212e337d9b4c951005c65d4e9ae1",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "f25998d0fcca7645f70f",
+    "url": "/mywebsite/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/mywebsite/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f25998d0fcca7645f70f",
+    "url": "/mywebsite/static/js/main.0916faa9.chunk.js"
+  },
+  {
+    "revision": "1029a0e86ffa031e1963",
+    "url": "/mywebsite/static/js/runtime~main.8e5ad122.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mywebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
